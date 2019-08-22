a84ffeb6a045a1409e5271a160afb5be268e8953
mahsa ghazi <mahsaghazi@mahsas-MacBook-Pro.local>
2019-08-22 11:07:35 +0430

    variable declaration

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    float salary;
+    char letter = 'z';
+    a = 8;
+    b = 3;
+    int sum = a + b ;
+    printf("\n hello world \n");
+    //printf('dd',sizeof(int));
+     printf("%d \n", sum);
+    printf("%f \n", salary);
+    printf("%c \n", letter);
+    return 0;
+}
